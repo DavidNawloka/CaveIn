5ceb9524902879192c58caa118e325a50d11832b
David Nawloka - PC <nawlokadavid@gmail.com>
2021-06-01 21:29:56 +0200

    Added initial main menu

---
+++ Assets/Scripts/Feedback/MainMenuUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace CaveIn.Feedback
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] AudioMixer rocksAudioMixer;
+        [SerializeField] AudioMixer playerAudioMixer;
+        public void SetRocksVolume(float volume)
+        {
+            rocksAudioMixer.SetFloat("masterVolume", volume);
+            if (volume == -30f)
+            {
+                rocksAudioMixer.SetFloat("masterVolume", -80f);
+            }
+        }
+        public void SetPlayerVolume(float volume)
+        {
+            playerAudioMixer.SetFloat("masterVolume", volume);
+            if (volume == -30f)
+            {
+                playerAudioMixer.SetFloat("masterVolume", -80f);
+            }
+        }
+        public void SetQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+    }
+}
