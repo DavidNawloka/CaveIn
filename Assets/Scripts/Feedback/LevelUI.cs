d7fc48bfbc76e34db71ac3076ffdff8ffc07eaff
David Nawloka - PC <nawlokadavid@gmail.com>
2021-06-04 20:49:29 +0200

    Revert "V1.1 finished"
    This reverts commit 6cab6b71a1ef620b8b65fe32f81d450d098d3991.

--- Assets/Scripts/Feedback/LevelUI.cs
+++ Assets/Scripts/Feedback/LevelUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e03c36bbfba98e6fd1841a1ee1d0bb257edcfc05bf48ce78a0851bd48eb5227
-size 2750
+oid sha256:94ebda6f88729238a0835b563ec26eca782a13ee61ecf260474e761232000f82
+size 2551
